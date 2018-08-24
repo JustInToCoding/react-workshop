84c41f6f18b161cc85d5587685ac5edd688c1e39
Justin Steenhuis <JUSTIN@SGRNET.local>
2018-08-24 10:42:15 +0200

    Make marker work properly with icon from node_modules

--- my-app/src/map/map.js
+++ my-app/src/map/map.js
@@ -2,8 +2,9 @@ import React, { Component, createRef } from 'react';
 
 import './map.css';
 import 'leaflet/dist/leaflet.css';
-import test from 'leaflet/dist/images/marker-icon.png';
-import test2 from 'leaflet/dist/images/marker-shadow.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerRetinaIcon from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import leaflet from 'leaflet';
 
@@ -21,15 +22,18 @@ class LeafletMap extends Component {
 
   constructor(props) {
     super(props);
+    console.log(leaflet.Icon.Default.prototype.options);
     this.icon = leaflet.icon({
-      iconUrl: test,
-      shadowUrl: test2,
+      iconUrl: markerIcon,
+      shadowUrl: markerShadow,
+      iconRetinaUrl: markerRetinaIcon,
   
-      iconSize:     [21, 41], // size of the icon
+      iconSize:     [25, 41], // size of the icon
       shadowSize:   [41, 41], // size of the shadow
-      iconAnchor:   [10, 0], // point of the icon which will correspond to marker's location
-      shadowAnchor: [4, 62],  // the same for the shadow
-      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+      iconAnchor:   [12, 41], // point of the icon which will correspond to marker's location
+      shadowAnchor: [12, 41],  // the same for the shadow
+      popupAnchor:  [1, -34], // point from which the popup should open relative to the iconAnchor
+      tooltipAnchor: [16, -28]
     });
   }
 
